76204fec2ae8b77b84b970a6b1bf390c1c7e5995
Haidar Afif Maulana <haidarafifmaulana@gmail.com>
2017-10-16 10:15:32 +0700

    auditable di kas dan kategori transaksi kas dan pencegahan hapus

--- app/Kas.php
+++ app/Kas.php
@@ -3,12 +3,53 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Session;
+use Yajra\Auditable\AuditableTrait;
+
 
 class Kas extends Model
 {
     //
 
+    use AuditableTrait;
+
     protected $fillable = ['kode_kas','nama_kas'];
+
+
+
+    //MODEL EVENT 
+	public static function boot() {
+		parent::boot();
+
+	    self::deleting(function($kas) {
+
+	      $kas_terpakai =  TransaksiKas::where('kas', $kas->id)->count();
+	      
+	      if ($kas_terpakai > 0) {
+
+	         $pesan_alert = 
+	               "<div class='container-fluid'>
+	                    <div class='alert-icon'>
+	                    <i class='material-icons'>error</i>
+	                    </div>
+	                    <b>Gagal : $kas->nama_kas Sudah Terpakai Tidak Boleh Di Hapus</b>
+	                </div>";
+
+	          Session:: flash("flash_notification", [
+	            "level"=>"danger",
+	            "message"=> $pesan_alert
+	            ]);
+	        return false;
+	      }
+	      else {
+
+	        return true;
+	      }
+	 
+	    
+	    });  
+
+	}// end model event
 
     
 }
--- app/KategoriTransaksi.php
+++ app/KategoriTransaksi.php
@@ -3,11 +3,50 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Session;
+use Yajra\Auditable\AuditableTrait;
 
 class KategoriTransaksi extends Model
 {
     //
-
+use AuditableTrait;
         protected $fillable = ['nama_kategori_transaksi'];
+
+
+        //MODEL EVENT 
+	public static function boot() {
+		parent::boot();
+
+	    self::deleting(function($kategori) {
+
+	      $kategori_masuk_terpakai =  KasMasuk::where('kategori', $kategori->id)->count();
+	      $kategori_keluar_terpakai =  KasKeluar::where('kategori', $kategori->id)->count();
+	      $kategori_terpakai = $kategori_masuk_terpakai + $kategori_keluar_terpakai;
+	      
+	      if ($kategori_terpakai > 0) {
+
+	         $pesan_alert = 
+	               "<div class='container-fluid'>
+	                    <div class='alert-icon'>
+	                    <i class='material-icons'>error</i>
+	                    </div>
+	                    <b>Gagal : $kategori->nama_kategori_transaksi Sudah Terpakai Tidak Boleh Di Hapus</b>
+	                </div>";
+
+	          Session:: flash("flash_notification", [
+	            "level"=>"danger",
+	            "message"=> $pesan_alert
+	            ]);
+	        return false;
+	      }
+	      else {
+
+	        return true;
+	      }
+	 
+	    
+	    });  
+
+	}// end model event
 
 }
